72c7b4cf397e19e28c85b7f34d4ae18cbafb29fc
saratkumar <msaratkumar3@gmail.com>
2025-02-18 17:51:26 +0800

    Update CpaJobStatusRepository.java

--- src/main/java/com/dbs/cpa_api/repository/CpaJobStatusRepository.java
+++ src/main/java/com/dbs/cpa_api/repository/CpaJobStatusRepository.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 public interface CpaJobStatusRepository extends JpaRepository<CpaJobStatus, Long> {
 
+    
     List<CpaJobStatus> findByBusinessDate(String businessDate);
+
+    @Query(value = "SELECT * FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY business_date, system, target ORDER BY id desc) AS rn FROM cpa_job_status WHERE business_date = :businessDate AND system IN :systems AND entity = :entity AND target IS NOT NULL) t WHERE rn = 1", nativeQuery = true)
+    List<CpaJobStatus> getJobStatusBySystems(String businessDate, String entity, String[] systems);
+);
 
 }
