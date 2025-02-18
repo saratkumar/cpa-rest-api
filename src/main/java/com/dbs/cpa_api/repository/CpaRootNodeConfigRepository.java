058451ce2813f8d848c3a592f8161cba8638268b
saratkumar <msaratkumar3@gmail.com>
2025-02-18 17:51:48 +0800

    Update CpaRootNodeConfigRepository.java

--- src/main/java/com/dbs/cpa_api/repository/CpaRootNodeConfigRepository.java
+++ src/main/java/com/dbs/cpa_api/repository/CpaRootNodeConfigRepository.java
@@ -3,10 +3,13 @@ package com.dbs.cpa_api.repository;
 import com.dbs.cpa_api.models.CpaRootNodeConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CpaRootNodeConfigRepository extends JpaRepository<CpaRootNodeConfig, Long> {
 
     public Optional<CpaRootNodeConfig> getCpaRootNodeConfigBySystem(String system);
+
+    List<CpaRootNodeConfig> findBySystemIn(String[] systems);
 
 }
