0702c3d56d7a64ccb887c9b114d651fb25da58cb
saratkumar <msaratkumar3@gmail.com>
2025-02-18 17:51:37 +0800

    Update CpaRawRepository.java

--- src/main/java/com/dbs/cpa_api/repository/CpaRawRepository.java
+++ src/main/java/com/dbs/cpa_api/repository/CpaRawRepository.java
@@ -6,10 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CpaRawRepository extends JpaRepository<CpaRaw, Long> {
 
-    @Query(value = "SELECT * FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY job_name, business_date, app_code ORDER BY updated_at DESC) AS rn FROM cpa_raw WHERE business_date = :businessDate AND app_code IN :appCode AND entity = :entity AND job_name IN :jobNames) t WHERE rn = 1", nativeQuery = true)
-    List<CpaRaw> findByBusinessDateAndAppCodeAndEntityAndJobName(@Param("businessDate") String businessDate, @Param("appCode") String[] appCode, @Param("entity") String entity, @Param("jobNames") List<String> jobNames);
+    @Query(value = "SELECT * FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY business_date, system, jobname ORDER BY updated_at DESC) AS rn FROM cpa_raw WHERE business_date = :businessDate AND system IN :appCode AND entity = :entity AND jobname IS NOT NULL) t WHERE rn = 1", nativeQuery = true)
+    List<CpaRaw> getCpaRaws(@Param("businessDate") String businessDate, @Param("appCode") String[] appCode, @Param("entity") String entity);
+
+    Optional<CpaRaw> findByBusinessDateAndAppCodeInAndEntityAndJobNameIn(String businessDate, String[] systems, String entity, List<String> jobNames);
+
+
+    @Query(value = "SELECT * FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY business_date, system, jobname ORDER BY updated_at DESC) AS rn FROM cpa_raw WHERE business_date = :businessDate AND system IN :appCode AND entity = :entity AND jobname in :jobName) t WHERE rn = 1", nativeQuery = true)
+    List<CpaRaw> getCpaRawsWithJobName(@Param("businessDate") String businessDate, @Param("appCode") String[] appCode, @Param("entity") String entity, @Param("jobName") List<String> jobName);
+
 
 }
