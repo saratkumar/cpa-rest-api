f568290c4925f626ca9fa339303ec3f8177f54d5
saratkumar <msaratkumar3@gmail.com>
2025-02-18 17:52:00 +0800

    Update CriticalPathAnalysisOutputDataRepository.java

--- src/main/java/com/dbs/cpa_api/repository/CriticalPathAnalysisOutputDataRepository.java
+++ src/main/java/com/dbs/cpa_api/repository/CriticalPathAnalysisOutputDataRepository.java
@@ -1,7 +1,6 @@
 package com.dbs.cpa_api.repository;
 
 import com.dbs.cpa_api.models.CriticalPathAnalysisOutputData;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,6 @@ import java.util.List;
 @Repository
 public interface CriticalPathAnalysisOutputDataRepository extends JpaRepository<CriticalPathAnalysisOutputData, Long> {
 
-    List<CriticalPathAnalysisOutputData> findTop10ByBusinessDateAndTargetAndEntityAndSourceSystemInOrderByDurationDesc(String businessDate, String jobName, String entity, String[] systems);
+    List<CriticalPathAnalysisOutputData> findTop10ByBusinessDateAndTargetAndEntityAndSysParamOrderByDurationDesc(String businessDate, String jobName, String entity, String sysParam);
 
 }
